4b5308fc5b4be9996b8056eb75290c39f14b168e
Christine Vinaviles <christine.vinaviles@gmail.com>
2021-11-30 22:06:13 +1100

    Refactor

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -17,40 +17,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    @IBAction func keyPressedC(_ sender: UIButton) {
-        playSound(key: "C")
-    }
-    
-    @IBAction func keyPressedD(_ sender: UIButton) {
-        playSound(key: "D")
-    }
-    
-    
-    @IBAction func keyPressedE(_ sender: UIButton) {
-        playSound(key: "E")
-    }
-    
-    @IBAction func keyPressedF(_ sender: UIButton) {
-        playSound(key: "F")
-    }
-    
-    @IBAction func keyPressedG(_ sender: UIButton) {
-        playSound(key: "G")
-    }
-    
-    @IBAction func keyPressedA(_ sender: UIButton) {
-        playSound(key: "A")
-    }
-    
-    @IBAction func keyPressedB(_ sender: UIButton) {
-        playSound(key: "B")
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(key: sender.titleLabel!.text!)
     }
     
     func playSound(key: String) {
         let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
-
